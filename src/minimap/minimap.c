089905d8b951cb08621bac24072626aad99c9adf
lseeger <lseeger@student.42.fr>
2025-06-27 14:34:28 +0200

    mininmap will now be printed, at the correct position

--- src/minimap/minimap.c
+++ src/minimap/minimap.c
@@ -6,7 +6,7 @@
 /*   By: lseeger <lseeger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 13:05:16 by lseeger           #+#    #+#             */
-/*   Updated: 2025/06/27 14:12:56 by lseeger          ###   ########.fr       */
+/*   Updated: 2025/06/27 14:31:23 by lseeger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ static void	print_cell(t_game *game, int x, int y)
 {
 	int	pixel_x;
 	int	pixel_y;
+	int	x_res;
+	int	y_res;
 
-	if (x < 0 || y < 0 || x >= game->width || y >= game->height
-		|| game->grid[y][x] != '1')
+	x_res = x + (int)game->player_x;
+	y_res = y + (int)game->player_y;
+	if (x_res < 0 || y_res < 0 || x_res >= game->width || y_res >= game->height
+		|| game->grid[y_res][x_res] != '1')
 		return ;
 	pixel_y = 0;
 	while (pixel_y < game->mi_cell_size)
@@ -26,8 +30,9 @@ static void	print_cell(t_game *game, int x, int y)
 		pixel_x = 0;
 		while (pixel_x < game->mi_cell_size)
 		{
-			mlx_put_pixel(game->img, pixel_x + x * game->mi_cell_size, pixel_y
-				+ y * game->mi_cell_size, game->floor.rgb);
+			mlx_put_pixel(game->img, pixel_x + (MINIMAP_CELLS_COUNT / 2 + x)
+				* game->mi_cell_size, pixel_y + (MINIMAP_CELLS_COUNT / 2 + y)
+				* game->mi_cell_size, game->floor.rgb);
 			pixel_x++;
 		}
 		pixel_y++;
@@ -45,8 +50,7 @@ void	print_minimap(t_game *game)
 		x = -MINIMAP_CELLS_COUNT / 2;
 		while (x < MINIMAP_CELLS_COUNT / 2 + 1)
 		{
-			print_cell(game, (int)(game->player_x) + x, (int)(game->player_y)
-				+ y);
+			print_cell(game, x, y);
 			x++;
 		}
 		y++;
