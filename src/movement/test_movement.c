4171e7e4f397f674555b113ce6d4113431893b0c
lseeger <lseeger@student.42.fr>
2025-06-27 17:16:27 +0200

    added sliding when movement direction is blocked

--- src/movement/test_movement.c
+++ src/movement/test_movement.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_movement.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlendle <mlendle@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: lseeger <lseeger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 14:24:14 by mlendle           #+#    #+#             */
-/*   Updated: 2025/06/25 11:45:00 by mlendle          ###   ########.fr       */
+/*   Updated: 2025/06/27 17:16:03 by lseeger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	test_movement(t_game *game, double new_x, double new_y)
 {
-	// normalize te player movement
 	if ((game->forward || game->backward) && (game->right || game->left))
 	{
 		new_x = (new_x - game->player_x) / M_SQRT2 + game->player_x;
@@ -26,15 +25,15 @@ int	test_movement(t_game *game, double new_x, double new_y)
 		game->player_y = new_y;
 		return (0);
 	}
-	// else if (game->grid[(int)new_y][(int)game->player_x] != '1')
-	// {
-	// 	game->player_y = new_y;
-	// 	return (0);
-	// }
-	// else if (game->grid[(int)new_x][(int)game->player_y] != '1')
-	// {
-	// 	game->player_x = new_x;
-	// 	return (0);
-	// }
+	if (game->grid[(int)game->player_y][(int)new_x] != '1')
+	{
+		game->player_x = new_x;
+		return (0);
+	}
+	if (game->grid[(int)new_y][(int)game->player_x] != '1')
+	{
+		game->player_y = new_y;
+		return (0);
+	}
 	return (1);
 }
