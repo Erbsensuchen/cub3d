fc363f4dfb4bc86c1348e1088330b3baf2396888
lseeger <lseeger@student.42.fr>
2025-06-30 15:42:58 +0200

    fixed minimap bug

--- src/minimap/minimap.c
+++ src/minimap/minimap.c
@@ -6,7 +6,7 @@
 /*   By: lseeger <lseeger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 13:05:16 by lseeger           #+#    #+#             */
-/*   Updated: 2025/06/27 16:09:28 by lseeger          ###   ########.fr       */
+/*   Updated: 2025/06/30 15:42:25 by lseeger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,7 @@ static int	get_pixel_pos_x(t_game *game, int x, int pixel_x)
 
 	rest_x = (game->player_x - (int)game->player_x) * game->mi_cell_size;
 	res = pixel_x + (MINIMAP_CELLS_COUNT / 2 + x) * game->mi_cell_size - rest_x;
-	if (res < 0 || res > game->mi_size)
-		return (0);
-	else
-		return (res);
+	return (res);
 }
 
 static int	get_pixel_pos_y(t_game *game, int y, int pixel_y)
@@ -32,10 +29,7 @@ static int	get_pixel_pos_y(t_game *game, int y, int pixel_y)
 
 	rest_y = (game->player_y - (int)game->player_y) * game->mi_cell_size;
 	res = pixel_y + (MINIMAP_CELLS_COUNT / 2 + y) * game->mi_cell_size - rest_y;
-	if (res < 0 || res > game->mi_size)
-		return (0);
-	else
-		return (res);
+	return (res);
 }
 
 static void	print_cell(t_game *game, int x, int y)
@@ -56,8 +50,11 @@ static void	print_cell(t_game *game, int x, int y)
 		pixel_x = 0;
 		while (pixel_x < game->mi_cell_size)
 		{
-			mlx_put_pixel(game->img, get_pixel_pos_x(game, x, pixel_x),
-				get_pixel_pos_y(game, y, pixel_y), game->floor.rgb);
+			x_res = get_pixel_pos_x(game, x, pixel_x);
+			y_res = get_pixel_pos_y(game, y, pixel_y);
+			if (x_res >= 0 && x_res < game->mi_size && y_res >= 0
+				&& y_res < game->mi_size)
+				mlx_put_pixel(game->img, x_res, y_res, game->floor.rgb);
 			pixel_x++;
 		}
 		pixel_y++;
